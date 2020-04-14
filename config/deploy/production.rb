b33252084e3f6ad5a538720682f0b86206b33ab8
thkernel <salut.amos@gmail.com>
2020-04-14 08:02:15 +0000

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-set :application, "redmine.prod"               # the name of your website - should also be the name of the directory
-set :deploy_to, "/home/ubuntu/rails-apps/redmine/redmine.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
+set :application, "pdfapp.prod"               # the name of your website - should also be the name of the directory
+set :deploy_to, "/home/ubuntu/rails-apps/pdfapp/pdfapp.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :rails_env, "production"
 
 #server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
